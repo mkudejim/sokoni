37f708638619020469d597049c93942f0b34375f
mkudejim <mkudejim@msu.edu>
2016-10-14 19:51:27 -0400

    Humble beginings

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html> 
+<head>
+	<link rel="stylesheet" href="sokoni.css">
+	<title>Sokoni</title>
+</head>
+
+<body>
+	<h1> Sokoni </h1>
+	<div class="header_bar">
+		<p>
+			<a href="index.php"> Home </a> |
+			<a href="profile.php"> Profile </a> |
+			<a href="orders.php"> Orders </a> |
+			<a href="advertising.php"> Advetising </a> |
+			<a href="about.php"> About </a>
+		</p>
+	</div>
+	
+	<div class="contents">
+		<div class = "buttons">
+			<p>
+				<a href="login.php">Login</a>
+			</p>
+		
+			<p>
+				<a href="signup.php">Sign up</a>
+			</p>
+		</div>
+	</div>
+</body>
+</html>
+
